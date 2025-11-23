4eee3225ecd542ca7ab91ad5e5044a070834a45f
yamganty vivekvardhan goud <vivekvardhan592@gmail.com>
2025-11-23 22:31:59 +0530

    Update API endpoint for chat analysis

--- Frontend/script.js
+++ Frontend/script.js
@@ -5,7 +5,7 @@ function showSections() {
 }
 
 async function analyzeChat(messages) {
-  const response = await fetch("http://127.0.0.1:8000/analyze-chat", {
+  const response = await fetch("https://EmpathyEngine-vivekvardhan592.hf.space/analyze-chat", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ messages })
